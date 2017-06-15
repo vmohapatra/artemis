56509553e11f52999b96e349b75f60cf6bdc46ac
vmohapatra <vmohapatra@expedia.com>
2017-06-15 09:28:31 -0700

    Adding the welcome view with autocomplete and datepicker

--- static_resources/js/client/client.js
+++ static_resources/js/client/client.js
@@ -20,6 +20,28 @@ $(document).ready(function(){
         }, 2000);
     }
     getWelcomeTagline();
+    
+    // Utility function to set the autocomplete suggestion data
+    function setSuggestions() {
+        var input = document.getElementById("ip_destination");
+        //data-list="Ada, Java, JavaScript, Brainfuck, LOLCODE, Node.js, Ruby on Rails"
+
+        // Show label but insert value into the input:
+        new Awesomplete(input, {
+            list: [
+                { label: "Belarus", value: "BY", rid: "12345" },
+                { label: "China", value: "CN", rid: "54321" },
+                { label: "United States", value: "US", rid: "67823" }
+            ],
+            options: {
+                selectionCallback : function(data) { 
+                    console.log("Typeahead selection happened and we got this back");
+                    console.log(data);
+                }
+            }
+        });
+    }
+    setSuggestions ();
     /*******************************************************
         Event Handlers
     *******************************************************/
